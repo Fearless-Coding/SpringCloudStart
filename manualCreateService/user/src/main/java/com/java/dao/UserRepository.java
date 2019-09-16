e019968c8afc06c5f98bad4c472ed184b5a4df0b
庞志阳 <66700800@qq.com>
2019-09-16 22:31:48 +0800

    no message

---
+++ manualCreateService/user/src/main/java/com/java/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.java.dao;
+
+import com.java.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
